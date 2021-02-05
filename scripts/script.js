e3d206f8c17c8fffca476a971b0bf59af9f0898c
Iana Burkovskaia <ianamerely@yandex.com>
2021-02-05 13:30:23 +0300

    fix: правки после 1 ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,19 @@
-const popup = document.querySelector('.popup')
-const popupOpenButton = document.querySelector('.profile__edit-button')
-const popupCloseButton = popup.querySelector('.popup__close-button')
-let nameInput = popup.querySelector('.popup__name-input');
-let jobInput = popup.querySelector('.popup__job-input');
+let popup = document.querySelector('.popup')
+let popupOpenButton = document.querySelector('.profile__edit-button')
+let popupCloseButton = popup.querySelector('.popup__close-button')
+let nameInput = popup.querySelector('.popup__input_type_name');
+let jobInput = popup.querySelector('.popup__input_type_job');
 let profileTitle = document.querySelector('.profile__title')
 let profileSubtitle = document.querySelector('.profile__subtitle')
-
-const addClassPopup = function(){
+let form = popup.querySelector('.popup__container');
+let addClassPopup = function(){
   popup.classList.add('popup_opened');
   nameInput.value= profileTitle.innerText
   jobInput.value = profileSubtitle.innerText
 }
-
-popupOpenButton.addEventListener('click', addClassPopup)
-
-const removeClassPopup = function(){
+let removeClassPopup = function(){
   popup.classList.remove('popup_opened');
 }
-
-popupCloseButton.addEventListener('click', removeClassPopup)
  
 function closeViaOverlay(evt){
   if (evt.target === evt.currentTarget){
@@ -26,12 +21,6 @@ function closeViaOverlay(evt){
   }
 }
 popup.addEventListener('click', closeViaOverlay)
-
-//пока все ок
-
-
-let form = popup.querySelector('.popup__container');
-let formSave = popup.querySelector('.popup__save-button');
 
 
 function formSubmitHandler (evt){
@@ -41,7 +30,9 @@ function formSubmitHandler (evt){
     removeClassPopup();
   }
 
-  form.addEventListener('submit', formSubmitHandler); 
+popupOpenButton.addEventListener('click', addClassPopup)
+popupCloseButton.addEventListener('click', removeClassPopup)
+form.addEventListener('submit', formSubmitHandler); 
 
 
 
