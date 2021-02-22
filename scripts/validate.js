c17bb74dfdf5d8e9b6a3f10e1638564dadc4ec05
Iana Burkovskaia <ianamerely@yandex.com>
2021-02-23 02:59:13 +0300

    feat: добавить валидацию полей. Выводить тексты ошибок. Настроить стили ошибок

---
+++ scripts/validate.js
@@ -0,0 +1,74 @@
+const validationSettings = {
+  formSelector: '.popup__container',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save-button',
+  submitButtonErrorSelector: 'popup__save-button_disabled',
+  inactiveButtonClass: '.popup__save-button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error_visible'
+}; 
+
+
+/*const hasInvalidInput = (inputList) =>{
+  return inputList.some((inputElement) =>{
+    return !inputElement.validity.valid;
+  })
+}*/
+
+const showInputError = (formElement, inputElement, errorMessage)=>{
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_visible')
+}
+
+const hideInputError =(formElement, inputElement)=>{
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+  errorElement.textContent = '';
+  errorElement.classList.remove('popup__input-error_visible')
+}
+
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+function setEventListeners(formElement) {
+  formElement.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+  });
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', (evt) => {
+      checkInputValidity(formElement, inputElement);
+    });
+  });
+}
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__container'));
+  formList.forEach((formElement) => {
+  setEventListeners(formElement)
+  });
+}
+
+/*const checkInputValidity = (formElement, inputElement) => {
+  /*const isInputElementNotValid = !inputElement.validity.valid;*/
+  /*if (isInputElementNotValid){
+    const errorMessage = inputElement.validationMessage
+    showInputError(formElement, inputElement, errorMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }*/
+  /*if (! inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+}*/
+
+
+enableValidation()
